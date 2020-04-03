6a6714390025391820000cb84a4e66b5f1e9976b
Jasper Hartong <jasperhartong+private@gmail.com>
2020-04-03 21:46:42 +0200

    quickly restore RSS (for testing)

--- pages/api/rss/[profile]/[playlist].ts
+++ pages/api/rss/[profile]/[playlist].ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 // simport { getPlaylist } from "../../../../src/api/collections/backend/adaptors/directus";
 import { podcastXMLFromFeed } from "../../../../src/utils/podcast";
+import { collectionsBackend } from "../../../../src/api/collection-storage/index";
 
 interface ErrorResponse {
   ok: false;
@@ -14,16 +15,14 @@ export default async (
   res: NextApiResponse<ErrorResponse | SuccessResponse>
 ) => {
   // For now use profile, should of should be based on profile + playlist
-  // const profile = req.query.profile;
-  // if (profile !== "elshartong") {
-  //   return res.status(404).json({ ok: false, msg: "Not Found" });
-  // }
-  // const feed = await getPlaylist(profile);
-  // if (feed) {
-  //   const podcastXML = podcastXMLFromFeed(profile, feed);
-  //   // TODO: Add CDN caching
-  //   res.setHeader("Content-type", "text/xml;charset=UTF-8");
-  //   return res.send(podcastXML);
-  // }
-  // return res.status(404).json({ ok: false, msg: "Not Found" });
+  const profile = req.query.profile as string;
+  const roomFetch = await collectionsBackend.getRoomBySlug(profile);
+  if (!roomFetch.ok) {
+    return res.status(404).json({ ok: false, msg: "Not Found" });
+  }
+
+  const podcastXML = podcastXMLFromFeed(profile, roomFetch.data.playlists[0]);
+  // TODO: Add CDN caching
+  // res.setHeader("Content-type", "text/xml;charset=UTF-8");
+  return res.send(podcastXML);
 };

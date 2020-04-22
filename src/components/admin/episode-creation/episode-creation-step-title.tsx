98cec990b9c01ce85b846dff98d277311454e47f
Jasper Hartong <jasperhartong+private@gmail.com>
2020-04-22 11:03:45 +0200

    Let form work

--- src/components/admin/episode-creation/episode-creation-step-title.tsx
+++ src/components/admin/episode-creation/episode-creation-step-title.tsx
@@ -1,9 +1,32 @@
+import { ReactNode } from "react";
 import { EpisodeCreationStepProps } from "./episode-creation-step-props";
 import AdminDualPaneLayout from "../layout/admin-dual-pane";
-import { Box, Typography, TextField, Button } from "@material-ui/core";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  FormGroup,
+} from "@material-ui/core";
 import IconNext from "@material-ui/icons/ChevronRight";
+import { useForm, Controller, ErrorMessage } from "react-hook-form";
 
 const EpisodeCreationStepTitle = (props: EpisodeCreationStepProps) => {
+  const defaultValues = { title: props.partialEpisode.title };
+  const { handleSubmit, control, formState, errors } = useForm<{
+    title: string;
+  }>({
+    mode: "onChange",
+    defaultValues,
+  });
+
+  const disabled = formState.isSubmitting;
+
+  const onSubmit = (formData: { title: string }) => {
+    props.onUpdate(formData);
+    props.onNext();
+  };
+
   return (
     <AdminDualPaneLayout
       title={props.playlist.title}
@@ -32,17 +55,38 @@ const EpisodeCreationStepTitle = (props: EpisodeCreationStepProps) => {
             Bijvoorbeeld de title van een hoofdstuk of een nummer dat aangeeft
             hoeveelste deel het is.
           </Typography>
-          <Box pt={2} pb={3}>
-            <TextField
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box pt={2} pb={2}>
+              <FormGroup>
+                {/* Title */}
+                <Controller
+                  control={control}
+                  rules={{ required: true }}
+                  as={TextField}
+                  label="Titel"
+                  placeholder="Titel aflevering"
+                  name="title"
+                  disabled={disabled}
+                />
+              </FormGroup>
+              <ErrorMessage
+                errors={errors}
+                name="title"
+                as={<ErrorMessageTypography />}
+                message="Vul een titel in"
+              />
+            </Box>
+            {/* submit */}
+            <Button
+              disabled={disabled}
+              type="submit"
+              variant="contained"
               fullWidth
-              placeholder="Titel aflevering"
-              // inputRef={inputRef}
-              defaultValue={props.partialEpisode.title}
-            />
-          </Box>
-          <Button variant="contained" fullWidth onClick={props.onNext}>
-            Neem intro op <IconNext />
-          </Button>
+            >
+              Neem intro op <IconNext />
+            </Button>
+          </form>
         </>
       }
     />
@@ -50,3 +94,9 @@ const EpisodeCreationStepTitle = (props: EpisodeCreationStepProps) => {
 };
 
 export default EpisodeCreationStepTitle;
+
+const ErrorMessageTypography = ({ children }: { children?: ReactNode }) => (
+  <Typography variant="subtitle2" color="error">
+    {children}
+  </Typography>
+);

b5779160f1956719469ec764b769a29533da03ee
Jasper Hartong <jasperhartong+private@gmail.com>
2020-04-24 11:01:30 +0200

    Correctly set timezone in parseDbDate

--- src/api/collection-storage/backends/directus-utils.ts
+++ src/api/collection-storage/backends/directus-utils.ts
@@ -3,4 +3,4 @@ import { IDateString } from "../../../app-schema/IDateString";
 
 const DIRECTUS_DATE_TIME_FORMAT = "y-MM-dd HH:mm:ss";
 export const parseDbDate = (date: IDateString): DateTime =>
-  DateTime.fromFormat(date, DIRECTUS_DATE_TIME_FORMAT);
+  DateTime.fromFormat(date, DIRECTUS_DATE_TIME_FORMAT, { zone: "UTC" });

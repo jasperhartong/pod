6b645f7965f8ab0246863ecef19be46946790a8f
Jasper Hartong <jasperhartong+private@gmail.com>
2020-04-26 21:58:28 +0200

    Readable duration example

--- pages/recorder.tsx
+++ pages/recorder.tsx
@@ -4,6 +4,7 @@ import { Button, Container } from "@material-ui/core";
 import { AudioRecorderVisualizer } from "../src/components/audio-recorder-hook/audio-recorder-visualizer";
 import FilePlayer from "react-player/lib/players/FilePlayer";
 import { formatBytes } from "../src/utils/audio-context";
+import { Duration } from "luxon";
 
 const Recorder = () => {
   const {
@@ -46,29 +47,29 @@ const Recorder = () => {
       ))}
       {dataType && <div>Recording in {dataType}</div>}
       {dataSize && <div>Recorded {formatBytes(dataSize)}</div>}
-      {dataSeconds && <div>Recorded {dataSeconds} seconds</div>}
+
+      <div>
+        Recorded{" "}
+        {Duration.fromObject({ seconds: dataSeconds }).toFormat("m:ss")}
+      </div>
+
       {isRecording && (
         <AudioRecorderVisualizer
           uniqueId="test"
           getFrequencyData={getFrequencyData}
         />
       )}
-      {/* {blobs.map((blob, index) => (
-        <audio key={index} src={URL.createObjectURL(blob)} controls></audio>
-      ))} */}
 
       {blob && <audio src={URL.createObjectURL(blob)} controls></audio>}
 
-      {/* <>
-        <FilePlayer
-          playing={playing}
-          url={[
-            "https://tapesme.s3.eu-central-1.amazonaws.com/08af0843-20e8-445a-a8cc-6e70d70da163.mp4",
-            "https://tapesme.s3.eu-central-1.amazonaws.com/46de95ba-5b4f-4b13-a5a0-bc4eb7afa79e.mp4",
-          ]}
-        />
-        <Button onClick={() => setPlaying(!playing)}>toggle playing</Button>
-      </> */}
+      {/* {blob && (
+        <>
+          <FilePlayer playing={playing} url={URL.createObjectURL(blob)} />
+          <Button onClick={() => setPlaying(!playing)}>
+            {playing ? "pause" : "play"}
+          </Button>
+        </>
+      )} */}
     </Container>
   );
 };

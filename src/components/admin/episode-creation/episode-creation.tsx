5e680810d432bda249fdb57f9b14bfcaddf71a94
Jasper Hartong <jasperhartong+private@gmail.com>
2020-05-01 12:53:52 +0200

    Setup correct form validation

--- src/components/admin/episode-creation/episode-creation.tsx
+++ src/components/admin/episode-creation/episode-creation.tsx
@@ -14,24 +14,39 @@ import { useForm, Controller, ErrorMessage } from "react-hook-form";
 import CloseIcon from "@material-ui/icons/Close";
 import { IRoom } from "../../../app-schema/IRoom";
 import { IPlaylist } from "../../../app-schema/IPlaylist";
+import useSignedMediaUploadDropZone from "../../../hooks/useSignedMediaUploadDropZone";
 
 interface Props {
   room: IRoom;
   playlist: IPlaylist;
 }
 
+interface FormValues {
+  title: string;
+  cover_file_url: string;
+}
+
 const EpisodeCreation = ({ room, playlist }: Props) => {
-  const { handleSubmit, control, formState, errors, watch } = useForm<{
-    title: string;
-  }>({
+  const formContext = useForm<FormValues>({
     mode: "onChange",
+    reValidateMode: "onChange",
+  });
+
+  const dropZone = useSignedMediaUploadDropZone({
+    onSuccess: (downloadUrl) => {
+      formContext.setValue("cover_file_url", downloadUrl, true);
+    },
+    acceptedMimeTypes: ["image/jpg", "image/jpeg"],
   });
 
   const defaultTitle = `Deel ${playlist.episodes.length + 1}`;
-  const watchedTitle = watch("title");
-  const SubmitDisabled = formState.isSubmitting;
+  const watchedTitle = formContext.watch("title");
+  const SubmitDisabled =
+    formContext.formState.isSubmitting || !formContext.formState.isValid;
 
-  const onSubmit = (formData: { title: string }) => {};
+  const onSubmit = (formData: FormValues) => {
+    console.warn(formData);
+  };
 
   return (
     <AdminDualPaneLayout
@@ -46,17 +61,22 @@ const EpisodeCreation = ({ room, playlist }: Props) => {
         </Link>
       }
       firstItem={
-        <Box
-          width="100%"
-          height="100%"
-          minHeight={200}
-          style={{
-            backgroundImage: `url("/background.png")`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-          }}
-        />
+        <div {...dropZone.getRootProps()}>
+          <input {...dropZone.getInputProps()} />
+          <Box
+            width="100%"
+            height="100%"
+            minHeight={200}
+            style={{
+              backgroundImage: `url("${
+                dropZone.downloadUrl || "/background.png"
+              }")`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          />
+        </div>
       }
       secondItem={
         <>
@@ -66,7 +86,7 @@ const EpisodeCreation = ({ room, playlist }: Props) => {
             hoeveelste deel het is.
           </Typography>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={formContext.handleSubmit(onSubmit)}>
             <Box pt={2} pb={2}>
               <FormGroup>
                 {/* Title */}
@@ -74,22 +94,27 @@ const EpisodeCreation = ({ room, playlist }: Props) => {
                   // set default value at least to a string to counter "uncontrolled to controlled error"
                   // https://github.com/react-hook-form/react-hook-form-website/issues/133
                   defaultValue={defaultTitle}
-                  control={control}
+                  control={formContext.control}
                   rules={{ required: true }}
                   as={TextField}
                   label="Titel"
                   placeholder="Titel aflevering"
                   name="title"
-                  disabled={SubmitDisabled}
+                  disabled={formContext.formState.isSubmitting}
                 />
               </FormGroup>
               <ErrorMessage
-                errors={errors}
+                errors={formContext.errors}
                 name="title"
                 as={<ErrorMessageTypography />}
                 message="Vul een titel in"
               />
             </Box>
+            <input
+              type="hidden"
+              ref={formContext.register({ required: true })}
+              name="cover_file_url"
+            />
             {/* submit */}
             <Button
               disabled={SubmitDisabled}

dd1dd65db60770a4677a553b3cbfb220c56a68ff
Jasper Hartong <jasperhartong+private@gmail.com>
2020-05-12 14:18:37 +0200

    Move listen-room to separate file

--- pages/rooms/[roomSlug]/index.tsx
+++ pages/rooms/[roomSlug]/index.tsx
@@ -1,19 +1,12 @@
+import { IResponse } from "@/api/IResponse";
 import roomFetch from "@/api/rpc/commands/room.fetch";
-import { IEpisode } from "@/app-schema/IEpisode";
 import { IRoom } from "@/app-schema/IRoom";
 import { LoaderCentered } from "@/components/admin/layout/loader-centered";
-import AppContainer from "@/components/app-container";
 import { ErrorPage } from "@/components/error-page";
-import PageFooter from "@/components/page-footer";
-import PlaylistGrid from "@/components/playlist-grid";
-import PlaylistHeader from "@/components/playlist-header";
-import SnackbarPlayer from "@/components/snackbar-player";
+import { ListenRoom } from "@/components/listen-room";
 import { useRouter } from "@/hooks/useRouter";
 import { useSWRRoom } from "@/hooks/useSWRRoom";
-import { Box, Grid, List, Typography } from "@material-ui/core";
 import { GetStaticPaths, GetStaticProps } from "next";
-import { useImmer } from "use-immer";
-import { IResponse } from "../../../src/api/IResponse";
 
 interface PageProps {
   preFetchedRoomResponse?: IResponse<IRoom>;
@@ -25,7 +18,6 @@ const ListenRoomPage = ({ preFetchedRoomResponse }: PageProps) => {
     router.isFallback ? null : (router.query.roomSlug as string),
     preFetchedRoomResponse
   );
-  const { playerState, start, stop, pause } = usePlayerState();
 
   if (!data || router.isFallback) {
     return <LoaderCentered />;
@@ -35,61 +27,7 @@ const ListenRoomPage = ({ preFetchedRoomResponse }: PageProps) => {
     return <ErrorPage error={data.error} />;
   }
 
-  const room = data.data;
-  const playingItem: IEpisode | undefined = findEpisodeById(
-    room,
-    playerState.playingEpisode?.episodeId
-  );
-
-  return (
-    <AppContainer>
-      <Box p={2}>
-        <Grid
-          container
-          spacing={1}
-          alignContent="center"
-          alignItems="center"
-          justify="space-between"
-          wrap="nowrap"
-        >
-          <Grid item>
-            <Typography variant="h4">{room.title}</Typography>
-          </Grid>
-        </Grid>
-      </Box>
-
-      {room.playlists
-        // Busines Logic: Only show playlists that contain published episodes
-        .filter(
-          (p) => p.episodes.filter((e) => e.status === "published").length > 0
-        )
-        .map((playlist) => (
-          <Box pb={4} key={playlist.id}>
-            <List>
-              <PlaylistHeader playlist={playlist} />
-            </List>
-            <Box p={2}>
-              <PlaylistGrid
-                playlist={playlist}
-                playingId={playerState.playingEpisode?.episodeId}
-                setPlayingId={start}
-                isPaused={Boolean(playerState.playingEpisode?.isPaused)}
-                setIsPaused={pause}
-              />
-            </Box>
-          </Box>
-        ))}
-
-      <PageFooter secondaryText={room.slug} />
-
-      <SnackbarPlayer
-        playingItem={playingItem}
-        isPaused={Boolean(playerState.playingEpisode?.isPaused)}
-        onPlayPause={pause}
-        onClose={stop}
-      />
-    </AppContainer>
-  );
+  return <ListenRoom room={data.data} />;
 };
 
 export default ListenRoomPage;
@@ -97,6 +35,7 @@ export default ListenRoomPage;
 export const getStaticProps: GetStaticProps<PageProps> = async ({ params }) => {
   return {
     props: {
+      // Runs serverside, call rpc handler directly
       preFetchedRoomResponse: await roomFetch.handle({
         slug: params?.roomSlug as string,
       }),
@@ -114,45 +53,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: [],
     fallback: true,
   };
-};
-
-const findEpisodeById = (room: IRoom, episodeId?: number) => {
-  return ([] as IEpisode[])
-    .concat(...[...room.playlists].map((playlist) => playlist.episodes))
-    .find((episode) => episode.id === episodeId);
-};
-
-export interface PlayerState {
-  playingEpisode?: {
-    episodeId: IEpisode["id"];
-    isPaused: boolean;
-  };
-}
-
-export const usePlayerState = () => {
-  const [state, dispatch] = useImmer<PlayerState>({});
-
-  const start = (episodeId: IEpisode["id"]) => {
-    dispatch((state) => {
-      state.playingEpisode = {
-        episodeId,
-        // always force not be paused when calling start again
-        isPaused: false,
-      };
-    });
-  };
-  const pause = (isPaused: boolean) => {
-    dispatch((state) => {
-      if (state.playingEpisode) {
-        state.playingEpisode.isPaused = isPaused;
-      }
-    });
-  };
-  const stop = () => {
-    dispatch((state) => {
-      state.playingEpisode = undefined;
-    });
-  };
-
-  return { playerState: state, start, pause, stop };
 };
---
+++ src/components/listen-room.tsx
@@ -0,0 +1,109 @@
+import { IEpisode } from "@/app-schema/IEpisode";
+import { IRoom } from "@/app-schema/IRoom";
+import { Box, Grid, List, Typography } from "@material-ui/core";
+import { useImmer } from "use-immer";
+import AppContainer from "./app-container";
+import PageFooter from "./page-footer";
+import PlaylistGrid from "./playlist-grid";
+import PlaylistHeader from "./playlist-header";
+import SnackbarPlayer from "./snackbar-player";
+
+export const ListenRoom = ({ room }: { room: IRoom }) => {
+  const { playerState, start, stop, pause } = usePlayerState();
+
+  const playingItem: IEpisode | undefined = findEpisodeById(
+    room,
+    playerState.playingEpisode?.episodeId
+  );
+
+  return (
+    <AppContainer>
+      <Box p={2}>
+        <Grid
+          container
+          spacing={1}
+          alignContent="center"
+          alignItems="center"
+          justify="space-between"
+          wrap="nowrap"
+        >
+          <Grid item>
+            <Typography variant="h4">{room.title}</Typography>
+          </Grid>
+        </Grid>
+      </Box>
+
+      {room.playlists
+        // Busines Logic: Only show playlists that contain published episodes
+        .filter(
+          (p) => p.episodes.filter((e) => e.status === "published").length > 0
+        )
+        .map((playlist) => (
+          <Box pb={4} key={playlist.id}>
+            <List>
+              <PlaylistHeader playlist={playlist} />
+            </List>
+            <Box p={2}>
+              <PlaylistGrid
+                playlist={playlist}
+                playingId={playerState.playingEpisode?.episodeId}
+                setPlayingId={start}
+                isPaused={Boolean(playerState.playingEpisode?.isPaused)}
+                setIsPaused={pause}
+              />
+            </Box>
+          </Box>
+        ))}
+
+      <PageFooter secondaryText={room.slug} />
+
+      <SnackbarPlayer
+        playingItem={playingItem}
+        isPaused={Boolean(playerState.playingEpisode?.isPaused)}
+        onPlayPause={pause}
+        onClose={stop}
+      />
+    </AppContainer>
+  );
+};
+
+const findEpisodeById = (room: IRoom, episodeId?: number) => {
+  return ([] as IEpisode[])
+    .concat(...[...room.playlists].map((playlist) => playlist.episodes))
+    .find((episode) => episode.id === episodeId);
+};
+
+interface PlayerState {
+  playingEpisode?: {
+    episodeId: IEpisode["id"];
+    isPaused: boolean;
+  };
+}
+
+const usePlayerState = () => {
+  const [state, dispatch] = useImmer<PlayerState>({});
+
+  const start = (episodeId: IEpisode["id"]) => {
+    dispatch((state) => {
+      state.playingEpisode = {
+        episodeId,
+        // always force not be paused when calling start again
+        isPaused: false,
+      };
+    });
+  };
+  const pause = (isPaused: boolean) => {
+    dispatch((state) => {
+      if (state.playingEpisode) {
+        state.playingEpisode.isPaused = isPaused;
+      }
+    });
+  };
+  const stop = () => {
+    dispatch((state) => {
+      state.playingEpisode = undefined;
+    });
+  };
+
+  return { playerState: state, start, pause, stop };
+};

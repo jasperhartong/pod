de89bacbbeb1c1e6a7965c497ef897ce6b3903f5
Jasper Hartong <jasperhartong+private@gmail.com>
2020-05-12 14:50:27 +0200

    Possible fix for Safari not releasing audio context.

--- src/components/snackbar-player.tsx
+++ src/components/snackbar-player.tsx
@@ -57,16 +57,23 @@ const SnackbarPlayer = ({
           !!playingItem && (
             <Box>
               <FilePlayer
-                url={playingItem.audio_file || undefined}
-                playing={!isPaused}
-                width="0px"
-                height="0px"
+                /* Show controls, but hide them, possible fix for Safari not releasing audio context on unmount */
+                width={0}
+                height={0}
+                controls={true}
+                style={{ display: "none" }}
                 config={{
                   file: { forceAudio: true },
                 }}
+                /* State */
+                playing={!isPaused}
+                url={playingItem.audio_file || undefined}
                 onProgress={({ played, loaded }) => {
                   setProgress(played * 100);
                   setDidLoad(loaded === 1);
+                }}
+                onEnded={() => {
+                  onPlayPause(true);
                 }}
               />
               <Grid container direction="column">

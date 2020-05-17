032a9f2237b90fb320637c21e3f39d9cadc92683
Jasper Hartong <jasperhartong+private@gmail.com>
2020-05-17 21:27:20 +0200

    remove reference to thumbnail

--- src/app-schema/IFileData.ts
+++ src/app-schema/IFileData.ts
@@ -1,16 +1,8 @@
 import * as t from "io-ts";
 
-const TThumbnail = t.type({
-  url: t.string,
-  relative_url: t.string,
-  dimension: t.string,
-  width: t.number,
-  height: t.number,
-});
-
 export const TImageData = t.type({
   full_url: t.string,
-  // thumbnails: t.array(TThumbnail),
+  //One day bring back thumbnails
 });
 
 export type IImageData = t.TypeOf<typeof TImageData>;

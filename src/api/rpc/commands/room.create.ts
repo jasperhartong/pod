7dfdb8724963970a95168d7e80d6a6d5072d57ff
Jasper Hartong <jasperhartong+private@gmail.com>
2020-05-27 10:21:39 +0200

    Open up room.create

--- src/api/rpc/commands/room.create.ts
+++ src/api/rpc/commands/room.create.ts
@@ -1,16 +1,10 @@
 import { dynamoTableTapes } from "@/api/collection-storage/backends/dynamodb";
 import { generateUid } from "@/api/collection-storage/backends/dynamodb/dynamodb-utils";
-import { ERR } from "@/api/IResponse";
 import { RPCHandlerFactory } from "@/api/rpc/rpc-server-handler";
-import HttpStatus from "http-status-codes";
 import { DateTime } from "luxon";
 import meta from "./room.create.meta";
 
 export default RPCHandlerFactory(meta, async (reqData) => {
-  if (reqData.secret !== "IGKjygsxlk") {
-    return ERR("No valid secret passed along", HttpStatus.FORBIDDEN);
-  }
-
   const uid = generateUid();
 
   return dynamoTableTapes.createRoom({

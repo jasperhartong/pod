779e29384394c843109ab4c4a946db8809676ba6
Jasper Hartong <jasperhartong+private@gmail.com>
2020-05-27 09:25:15 +0200

    Use built in path.extname

--- src/api/rpc/commands/signedurl.create.ts
+++ src/api/rpc/commands/signedurl.create.ts
@@ -1,21 +1,13 @@
 import { fileStorageBackend } from "@/api/file-storage/index";
 import { RPCHandlerFactory } from "@/api/rpc/rpc-server-handler";
+import path from "path";
 import uuid4 from "uuid4";
 import meta from "./signedurl.create.meta";
 
-const getFileExtension = (fileName: string): string | undefined => {
-  let extension = undefined;
-  if (fileName.includes(".")) {
-    extension = fileName.substr(fileName.lastIndexOf(".") + 1);
-  }
-  return extension;
-};
-
 export default RPCHandlerFactory(meta, async (reqData) => {
-  const originalExtension = getFileExtension(reqData.fileName);
-  const obscuredUniqueFileName = originalExtension
-    ? `${uuid4()}.${originalExtension}`
-    : uuid4();
+  const originalExtension = path.extname(reqData.fileName); // returns empty string if fails
+  const obscuredUniqueFileName = `${uuid4()}${originalExtension}`;
+
   // Not using original reqData.fileName for now, perhaps add it as tag on file later
   return await fileStorageBackend.getSignedUrl(
     obscuredUniqueFileName,

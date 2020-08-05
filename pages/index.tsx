1bc9b3f9ded974e962fb91787a328ab0582d3f8a
Jasper Hartong <jasperhartong@gmail.com>
2020-08-05 11:10:44 +0200

    Add index.tsx to not show file explorer on root

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import AppContainer from "@/components/app-container";
+import PageFooter from "@/components/page-footer";
+
+export default () => <AppContainer>
+    <PageFooter secondaryText="Nothing to see here" />
+</AppContainer>

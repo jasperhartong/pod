b65b467bb22a800d39c2e8655fa790429b424c0c
Jasper Hartong <jasperhartong@gmail.com>
2020-09-28 15:07:42 +0200

    Add DocsLayout component

---
+++ src/components/docs/docs-layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import AppContainer from "../app-container";
+
+export const DocsLayout = ({ children }: { children: ReactNode }) =>
+    <AppContainer maxWidth="sm">
+        <div className="docs-page">
+            {children}
+            <div style={{ paddingBottom: 32 }} />
+            <hr />
+            <a href="https://twitter.com/jasperhartong" target="_Blank"> @jasperhartong</a>
+
+            <p>
+                <sub>©2020</sub>
+            </p>
+        </div>
+    </AppContainer>
